38938a6b877505b83ab128b1bfc9cea7c03ff8a9
SuperConsole <superconsoledjem@gmail.com>
2017-11-13 20:19:52 +0900

    Update ipLogger.java

--- ipLogger.java
+++ ipLogger.java
@@ -1,8 +1,9 @@
-//このファイルはダミー。見ればわかるがそのままでは使えない。
+// このファイルはダミー。見ればわかるがそのままでは使えない。
 import java.util.*;//Vectorを使うためインポート
 class ipLogger{
-	//ここから下をコピ、Server.javaのrun()内のtry前にペ。
-	//Serverクラスのフィールドに静的VectorクラスipLoggerを宣言(インスタンスを生成)して組み込む。
+	// ここから下をコピ、Server.javaのrun()内のtry前にペ。
+	// kawashi/JavaSocketTest2のリポジトリからpullした場合はAnimServer0.java内の37~41行目を上書き。
+	// Serverクラスのフィールドに静的VectorクラスipLoggerを宣言(インスタンスを生成)して組み込む。
 	String ipAddress = s.getInetAddress().getHostAddress();
 	int connection_count=0;
 	ipLogger.add(ipAddress);
