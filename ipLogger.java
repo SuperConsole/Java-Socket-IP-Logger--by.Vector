9445383947b11882950c5d8be6e70caaf900d7c3
SuperConsole <superconsoledjem@gmail.com>
2017-11-13 20:08:11 +0900

    Update ipLogger.java

--- ipLogger.java
+++ ipLogger.java
@@ -1,7 +1,8 @@
 //このファイルはダミー。見ればわかるがそのままでは使えない。
-import java.util.*;
+import java.util.*;//Vectorを使うためインポート
 class ipLogger{
-	//ここから下をコピ、Server.javaのrun()内のtry前にペ。Serverクラスのフィールドに静的VectorクラスipLoggerを宣言して組み込む。
+	//ここから下をコピ、Server.javaのrun()内のtry前にペ。
+	//Serverクラスのフィールドに静的VectorクラスipLoggerを宣言(インスタンスを生成)して組み込む。
 	String ipAddress = s.getInetAddress().getHostAddress();
 	int connection_count=0;
 	ipLogger.add(ipAddress);
